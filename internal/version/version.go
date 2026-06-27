881674d4f244d39916e22afe2c3594bb0b764167
Smart <1014417486@qq.com>
2026-06-27 17:27:11 +0800

    chore: bump AppVersion to v1.0.0-beta.2

--- internal/version/version.go
+++ internal/version/version.go
@@ -8,7 +8,7 @@ package version
 
 var (
 	// AppVersion 是应用程序的版本号
-	AppVersion = "v1.0.0-beta.1"
+	AppVersion = "v1.0.0-beta.2"
 
 	// BuildCommit 是构建时的 git commit hash（通过 -ldflags 注入）
 	BuildCommit = "unknown"
