5326424ada2204c8d55e7da9f4f26b158ec64991
smartos <1014417486@qq.com>
2026-09-04 08:59:19 +0800

    chore: 版本号 v1.0.0-beta.22

--- internal/version/version.go
+++ internal/version/version.go
@@ -7,7 +7,7 @@ package version
 
 var (
 	// AppVersion 是应用程序的版本号
-	AppVersion = "v1.0.0-beta.21"
+	AppVersion = "v1.0.0-beta.22"
 
 	// BuildCommit 是构建时的 git commit hash（通过 -ldflags 注入）
 	BuildCommit = "unknown"
